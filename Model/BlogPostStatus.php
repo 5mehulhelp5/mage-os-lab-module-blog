cdd2fbfec8d267ef371563d229fbf1037ddfc94f
David Lambauer <david@run-as-root.sh>
2026-04-19 18:52:04 +0200

    feat(model): BlogPostStatus enum with unit tests

---
+++ Model/BlogPostStatus.php
@@ -0,0 +1,12 @@
+<?php
+declare(strict_types=1);
+
+namespace MageOS\Blog\Model;
+
+enum BlogPostStatus: int
+{
+    case Draft = 0;
+    case Scheduled = 1;
+    case Published = 2;
+    case Archived = 3;
+}
---
+++ Test/Unit/Model/BlogPostStatusTest.php
@@ -0,0 +1,41 @@
+<?php
+declare(strict_types=1);
+
+namespace MageOS\Blog\Test\Unit\Model;
+
+use MageOS\Blog\Model\BlogPostStatus;
+use PHPUnit\Framework\Attributes\Test;
+use PHPUnit\Framework\TestCase;
+
+final class BlogPostStatusTest extends TestCase
+{
+    #[Test]
+    public function draft_is_zero(): void
+    {
+        self::assertSame(0, BlogPostStatus::Draft->value);
+    }
+
+    #[Test]
+    public function scheduled_is_one(): void
+    {
+        self::assertSame(1, BlogPostStatus::Scheduled->value);
+    }
+
+    #[Test]
+    public function published_is_two(): void
+    {
+        self::assertSame(2, BlogPostStatus::Published->value);
+    }
+
+    #[Test]
+    public function archived_is_three(): void
+    {
+        self::assertSame(3, BlogPostStatus::Archived->value);
+    }
+
+    #[Test]
+    public function can_build_from_int(): void
+    {
+        self::assertSame(BlogPostStatus::Scheduled, BlogPostStatus::from(1));
+    }
+}
