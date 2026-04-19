ca505f73e5d7b836a093763f5432235d1885f6e8
David Lambauer <david@run-as-root.sh>
2026-04-19 18:47:03 +0200

    feat(api): PostRepositoryInterface

---
+++ Api/PostRepositoryInterface.php
@@ -0,0 +1,28 @@
+<?php
+declare(strict_types=1);
+
+namespace MageOS\Blog\Api;
+
+use MageOS\Blog\Api\Data\PostInterface;
+use MageOS\Blog\Api\Data\PostSearchResultsInterface;
+use Magento\Framework\Api\SearchCriteriaInterface;
+use Magento\Framework\Exception\CouldNotSaveException;
+use Magento\Framework\Exception\NoSuchEntityException;
+
+interface PostRepositoryInterface
+{
+    public function save(PostInterface $post): PostInterface;
+
+    /** @throws NoSuchEntityException */
+    public function getById(int $id): PostInterface;
+
+    /** @throws NoSuchEntityException */
+    public function getByUrlKey(string $urlKey, int $storeId): PostInterface;
+
+    public function getList(SearchCriteriaInterface $criteria): PostSearchResultsInterface;
+
+    public function delete(PostInterface $post): bool;
+
+    /** @throws NoSuchEntityException */
+    public function deleteById(int $id): bool;
+}
