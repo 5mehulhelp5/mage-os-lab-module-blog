2e49cdcecb17bb94a08e068b8da7755c32308a31
David Lambauer <david@run-as-root.sh>
2026-04-19 18:47:20 +0200

    feat(api): CategoryRepositoryInterface

---
+++ Api/CategoryRepositoryInterface.php
@@ -0,0 +1,28 @@
+<?php
+declare(strict_types=1);
+
+namespace MageOS\Blog\Api;
+
+use MageOS\Blog\Api\Data\CategoryInterface;
+use MageOS\Blog\Api\Data\CategorySearchResultsInterface;
+use Magento\Framework\Api\SearchCriteriaInterface;
+use Magento\Framework\Exception\CouldNotSaveException;
+use Magento\Framework\Exception\NoSuchEntityException;
+
+interface CategoryRepositoryInterface
+{
+    public function save(CategoryInterface $category): CategoryInterface;
+
+    /** @throws NoSuchEntityException */
+    public function getById(int $id): CategoryInterface;
+
+    /** @throws NoSuchEntityException */
+    public function getByUrlKey(string $urlKey, int $storeId): CategoryInterface;
+
+    public function getList(SearchCriteriaInterface $criteria): CategorySearchResultsInterface;
+
+    public function delete(CategoryInterface $category): bool;
+
+    /** @throws NoSuchEntityException */
+    public function deleteById(int $id): bool;
+}
