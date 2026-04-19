e26f75ad804f8d00aa0058fbd350cef2da458ffc
David Lambauer <david@run-as-root.sh>
2026-04-19 18:46:37 +0200

    feat(api): search results interfaces for all four entities

---
+++ Api/Data/CategorySearchResultsInterface.php
@@ -0,0 +1,19 @@
+<?php
+declare(strict_types=1);
+
+namespace MageOS\Blog\Api\Data;
+
+use Magento\Framework\Api\SearchResultsInterface;
+
+interface CategorySearchResultsInterface extends SearchResultsInterface
+{
+    /**
+     * @return CategoryInterface[]
+     */
+    public function getItems(): array;
+
+    /**
+     * @param CategoryInterface[] $items
+     */
+    public function setItems(array $items): self;
+}
---
+++ Api/Data/TagSearchResultsInterface.php
@@ -0,0 +1,19 @@
+<?php
+declare(strict_types=1);
+
+namespace MageOS\Blog\Api\Data;
+
+use Magento\Framework\Api\SearchResultsInterface;
+
+interface TagSearchResultsInterface extends SearchResultsInterface
+{
+    /**
+     * @return TagInterface[]
+     */
+    public function getItems(): array;
+
+    /**
+     * @param TagInterface[] $items
+     */
+    public function setItems(array $items): self;
+}
