2cc3ce8cf0ba982e038416f426d8cb2e1874eb00
David Lambauer <david@run-as-root.sh>
2026-04-20 12:00:05 +0200

    feat(admin): Tag form DataProvider + NewAction/Edit controllers + layouts

---
+++ Ui/DataProvider/Tag/Form/DataProvider.php
@@ -0,0 +1,60 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MageOS\Blog\Ui\DataProvider\Tag\Form;
+
+use Magento\Framework\App\RequestInterface;
+use Magento\Ui\DataProvider\AbstractDataProvider;
+use MageOS\Blog\Model\ResourceModel\Tag\CollectionFactory;
+
+class DataProvider extends AbstractDataProvider
+{
+    /**
+     * @var array<int, array<string, mixed>>
+     */
+    protected $loadedData = [];
+
+    /**
+     * @param array<string, mixed> $meta
+     * @param array<string, mixed> $data
+     */
+    public function __construct(
+        string $name,
+        string $primaryFieldName,
+        string $requestFieldName,
+        CollectionFactory $collectionFactory,
+        private readonly RequestInterface $request,
+        array $meta = [],
+        array $data = []
+    ) {
+        parent::__construct($name, $primaryFieldName, $requestFieldName, $meta, $data);
+        $this->collection = $collectionFactory->create();
+    }
+
+    /**
+     * @return array<int, array<string, mixed>>
+     */
+    public function getData(): array
+    {
+        if ($this->loadedData !== []) {
+            return $this->loadedData;
+        }
+
+        $tagId = (int) $this->request->getParam('tag_id');
+        if ($tagId <= 0) {
+            return $this->loadedData;
+        }
+
+        $this->collection->addFieldToFilter('tag_id', (string) $tagId);
+
+        foreach ($this->collection->getItems() as $tag) {
+            $id = (int) $tag->getId();
+            $this->loadedData[$id] = [
+                'tag' => $tag->getData(),
+            ];
+        }
+
+        return $this->loadedData;
+    }
+}
